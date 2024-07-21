7df4f8341610aef45cd0819eff1313f7147991b2
aliallam98 <allam98.ali@gmail.com>
2024-07-21 10:52:06 +0300

    Finished task three .

---
+++ taskThree.js
@@ -0,0 +1,39 @@
+// 1-
+const sumArrayOfNumbers = (arrayOfNumber) => {
+  return arrayOfNumber.reduce((acc, curr) => acc + curr, 0);
+};
+// ---------------------------------------
+
+
+// 2-
+const findBiggestNumber = (arrayOfNumber) => {
+  return Math.max(...arrayOfNumber);
+};
+const findBiggestNumberTwo = (arrayOfNumber) => {
+  return arrayOfNumber.sort((a, b) => b - a)[0]
+};
+// ---------------------------------------
+
+
+
+// 3-
+const reverseText = (text)=>{
+    return text.split("").reverse().join("")
+}
+// ---------------------------------------
+
+
+
+// 4-
+const removeDuplicatedNumbers = (arrayOfNumber)=>{
+    return [...new Set(arrayOfNumber)]
+}
+
+console.log(sumArrayOfNumbers([1,2,2,4,4,5]));
+console.log(findBiggestNumber([1,2,2,4,4,5]));
+console.log(findBiggestNumberTwo([1,2,2,4,4,5]));
+console.log(reverseText("Ali Allam"));
+console.log(removeDuplicatedNumbers([1,2,2,4,4,5]));
+
+
+// i used node js to run this file 
